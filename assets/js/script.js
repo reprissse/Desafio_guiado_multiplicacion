6663599a82575d5a82f677904fc652a8c5490d92
Raquel Jimenez <raquel.jimenez.ramirez@gmail.com>
2024-02-10 22:52:16 -0300

    :sparkles: se ingresa validación de numero 1 a 20

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,13 @@ var numero = parseInt(prompt('Ingresa un número:'));
 if (validarNumero(numero)) {
     generarTablasYFactoriales(numero);
 }
+
+// Valida que el numero esté entre 1 y 20
+function validarNumero(numero) {
+    if (numero >= 1 && numero <= 20) {
+        return true; // El número es válido
+    } else {
+        console.log("Por favor, ingresa un número entre 1 y 20.");
+        return false; // El número no es válido
+    }
+}
